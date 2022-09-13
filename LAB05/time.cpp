c79126a5364b45d90e5bc1bda26cd885805fa069
Soumyadeep Chandra <soumyadeepchandra2002@gmail.com>
2022-09-13 15:48:20 +0530

    Updated files

--- LAB05/time.cpp
+++ LAB05/time.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 class time_format
@@ -6,39 +6,57 @@ class time_format
 private:
     int hh;
     int mm;
+
 public:
     time_format()
     {
-        hh=0;
-        mm=0;
-        cout<<hh<<":"<<mm<<endl;
+        hh = 0;
+        mm = 0;
+        cout << hh << ":" << mm << endl;
     }
-    time_format(int h,int m)
+    time_format(int h)
     {
-        hh=h;
-        mm=m;
-        cout<<hh<<":"<<mm<<endl;
+        hh = h;
+        mm = 0;
+        cout << hh << ":" << mm << endl;
+    }
+    time_format(int h, int m)
+    {
+        hh = h;
+        mm = m;
+        cout << hh << ":" << mm << endl;
     }
     time_format(time_format &t)
     {
-        hh=t.hh;
-        mm=t.mm;
-        cout<<hh<<":"<<mm<<endl;
+        hh = t.hh;
+        mm = t.mm;
+        cout << hh << ":" << mm << endl;
     }
-    ~time_format()
-    {
-        cout<<hh<<" and "<<mm<<" are destroyed"<<endl;
-    };
 };
-
 
 int main()
 {
-    int h,m;
-    cout<<"Enter hours and minutes: ";
-    cin>>h>>m;
-    
-    time_format t1;
-    time_format t2(h,m);
-    time_format t3(t2);
+    int h, m, n;
+
+    printf("Enter your choice: \n1.To enter hours and mins\n2.To enter only hours \n");
+    scanf("%d", &n);
+
+    if (n == 1)
+    {
+        cout << "Enter hours and minutes: ";
+        cin >> h >> m;
+        time_format t1;
+        time_format t2(h, m);
+        time_format t3(t2);
+    }
+    else if (n == 2)
+    {
+        cout << "Enter hours: ";
+        cin >> h;
+        time_format t1;
+        time_format t2(h);
+        time_format t3(t2);
+    }
+    else
+        printf("Invalid choice!");
 }
